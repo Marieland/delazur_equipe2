0c02de00b7f4d555d0853147910145af64cb32f8
Gabtaca <gabrieltaca117@gmail.com>
2024-03-17 17:35:37 -0400

    Disposition réglée. Animation de coeur rajouter a coté du compteur. tite criss de flèche jaune est rendu blanche.

--- assets/JS/spectacles.js
+++ assets/JS/spectacles.js
@@ -1,13 +1,41 @@
+// Dompter les clics sur le coeur.
 let heartCount = 0;
 
+// Fonction appelée lorsqu'un bouton de coeur est cliqué.
 function changeHeart(event) {
-    var heart = event.target;
-    if (heart.src.match(/heart_empty/)) {
-        heart.src = "/assets/img/heart_full.png";
+    // Sélection de l'icône de coeur à partir de l'élément cliqué.
+    const heartIcon = event.currentTarget.querySelector('.heart__icon');
+    // Sélection de l'élément contenant le compteur de clics.
+    const count = document.querySelector(".count");
+    
+    // Vérification si l'icône de cœur est vide ou pas.
+    if (heartIcon.src.includes('heart_empty')) {
+        // Si l'icône est vide, la remplacer par l'icône pleine et ajouter le compte au compteur.
+        heartIcon.src = "/assets/img/heart_full.png";
         heartCount++;
     } else {
-        heart.src = "/assets/img/heart_empty.png";
+        // Sinon, remplacer l'icône pleine par l'icône vide et décrémenter le compteur.
+        heartIcon.src = "/assets/img/heart_empty.png";
         heartCount--;
     }
-    document.querySelector(".spectacles__aimes").textContent = `Nombre de spectacles aimés → ${heartCount}`;
+    
+    // Mettre à jour le texte du compteur avec la nouvelle valeur.
+    count.textContent = heartCount;
+
+    // Créer un élément image pour l'animation du cœur qui apparaît.
+    const popHeart = document.createElement('img');
+    popHeart.src = '/assets/img/heart_full.png';
+    popHeart.classList.add('pop-heart');
+    // Ajouter l'élément à l'élément contenant le compteur.
+    count.appendChild(popHeart);
+    
+    // Définir un délai pour l'animation d'apparition.
+    setTimeout(() => {
+        popHeart.style.opacity = '1';
+        // Le délai pour l'animation qui fait disparaître le coeur.
+        setTimeout(() => {
+            popHeart.style.opacity = '0';
+            popHeart.remove();
+        }, 500);
+    }, 100);
 }
