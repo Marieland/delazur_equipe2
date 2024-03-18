2c14165fbbf6acf756fa5f69c363fd7dab3e3adc
Gabtaca <gabrieltaca117@gmail.com>
2024-03-17 22:22:12 -0400

    petit changement sur l'animation du coeur

--- assets/JS/spectacles.js
+++ assets/JS/spectacles.js
@@ -1,4 +1,4 @@
-// Dompter les clics sur le coeur.
+// Compter les clics sur le coeur.
 let heartCount = 0;
 
 // Fonction appelée lorsqu'un bouton de coeur est cliqué.
@@ -13,29 +13,35 @@ function changeHeart(event) {
         // Si l'icône est vide, la remplacer par l'icône pleine et ajouter le compte au compteur.
         heartIcon.src = "/assets/img/heart_full.png";
         heartCount++;
+
+        // Mettre à jour le texte du compteur avec la nouvelle valeur.
+        count.textContent = heartCount;
+
+        // Créer un élément image pour l'animation du cœur qui apparaît.
+        const popHeart = document.createElement('img');
+        popHeart.src = '/assets/img/heart_full.png';
+        popHeart.classList.add('pop-heart');
+        // Ajouter l'image de coeur à l'élément contenant le compteur.
+        count.appendChild(popHeart);
+        
+        // Définir un délai pour l'animation d'apparition.
+        setTimeout(() => {
+            popHeart.style.opacity = '1';
+            // Le délai pour l'animation qui fait disparaître le coeur.
+            setTimeout(() => {
+                popHeart.style.opacity = '0';
+                // Supprimer l'élément après l'animation de disparition.
+                setTimeout(() => {
+                    popHeart.remove();
+                }, 500);
+            }, 500);
+        }, 100);
     } else {
-        // Sinon, remplacer l'icône pleine par l'icône vide et décrémenter le compteur.
+        // Sinon, remplacer l'icône pleine par l'icône vide et reduire le compte.
         heartIcon.src = "/assets/img/heart_empty.png";
         heartCount--;
-    }
-    
-    // Mettre à jour le texte du compteur avec la nouvelle valeur.
-    count.textContent = heartCount;
 
-    // Créer un élément image pour l'animation du cœur qui apparaît.
-    const popHeart = document.createElement('img');
-    popHeart.src = '/assets/img/heart_full.png';
-    popHeart.classList.add('pop-heart');
-    // Ajouter l'élément à l'élément contenant le compteur.
-    count.appendChild(popHeart);
-    
-    // Définir un délai pour l'animation d'apparition.
-    setTimeout(() => {
-        popHeart.style.opacity = '1';
-        // Le délai pour l'animation qui fait disparaître le coeur.
-        setTimeout(() => {
-            popHeart.style.opacity = '0';
-            popHeart.remove();
-        }, 500);
-    }, 100);
+        // Mettre à jour le texte du compteur avec la nouvelle valeur.
+        count.textContent = heartCount;
+    }
 }
